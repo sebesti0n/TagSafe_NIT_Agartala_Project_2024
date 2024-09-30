cb8697a655c1d313da3622db62f7920a4d8c3693
kavikhalique <kavikhalique3@gmail.com>
2024-09-30 21:50:42 +0530

    resolved popup issue on map marker

--- web/admin-panel-frontend/src/components/Dashboard/components/SearchCameras/markers/index.js
+++ web/admin-panel-frontend/src/components/Dashboard/components/SearchCameras/markers/index.js
@@ -1,15 +1,43 @@
 import { Marker, Popup } from "react-leaflet"
-import useFetchOwnerDetails from "../hooks/useFetchOwnerDetails"
 import { CircularProgress, Typography } from "@mui/material";
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
 
 const CustomPopup = ({ownerId})=>{
 
-    const {
-        loading,
-        error,
-        ownerDets,
-    } = useFetchOwnerDetails(ownerId);
+  const [loading,setLoading] = useState(false);
+  const [error,setError] = useState("");
+  const [ownerDets,setOwnerDets] = useState({name:"",address:"",phone:""});
+
+  const fetchOwnerDetails = async()=>{
+      setLoading(true);
+      try{
+          const token = localStorage.getItem("auth");
+          const response = await axios.get(process.env.REACT_APP_API_ENDPOINT+"admin/camOwner?uid="+ownerId,{
+              headers:{
+                  authorizations:token,
+              }
+          })
+          if(response?.status===200){
+              const { data } = response;
+              setOwnerDets({
+                  name:data?.owner[0]?.Name,
+                  address:data?.owner[0]?.addressLine1,
+                  phone:data?.owner[0]?.phoneNumber
+              })
+          }
+      }
+      catch(e){
+          console.log(e?.message);
+          setError(e?.message)
+      }
+      setLoading(false);
+  }
+
+  useEffect(()=>{
+      fetchOwnerDetails();
+  },[])
 
     return (
         <Popup>
